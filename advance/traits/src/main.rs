7a49c2f3fee94c407c27766a68827caade0ab3eb
Aslam Sarfraz <aslam91r@gmail.com>
2021-01-15 00:20:32 +0500

    Add Trait Bounds

--- advance/traits/src/main.rs
+++ advance/traits/src/main.rs
@@ -1,4 +1,4 @@
-// Traits, Default Traits, Multiple Methods
+// Traits, Default Traits, Multiple Methods, Trait Bounds
 
 #[derive(Debug)]
 struct Student{
@@ -21,6 +21,7 @@ pub trait FullName{
 }
 
 impl FullName for Student{}
+
 impl FullName for Teacher{
     fn fulnam(&self)->String{
         format!("{} {}", self.Firstname, self.Lastname)
@@ -29,6 +30,24 @@ impl FullName for Teacher{
         format!("{}", self.Lastname)
     }
 }
+
+// Traits Bound
+
+// Simple Trait Bound
+pub fn notify<T: FullName>(Item: T){
+    println!("{}", Item.fulnam());
+}
+
+// This trait bound is use when we strickly use same datatypes
+pub fn notify2<T: FullName>(ItemOne: T , itemTwo: T){
+    println!("{} and {}", ItemOne.fulnam(), itemTwo.lastnam());
+}
+
+// This Trait Bound is use when we want to pass different datatypes
+pub fn notify3(itemOne: impl FullNamem, itemTwo: impl FullName){
+    println!("{} and {}", itemOne.fulnam, itemTwo.lastnam)
+}
+
 fn main() {
 
     // Simple Traits
@@ -53,4 +72,8 @@ fn main() {
     
     println!("{}",Aslam.fulnam());
     println!("{}",Laila.lastnam());
+
+    // Simple Using trait bound
+    notify(Aslam);
+    notify(Laila);
 }
